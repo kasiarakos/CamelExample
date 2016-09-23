37af91fb8351e4f3d2f0ee6731978adbb38d1926
Dimitris Kasiaras <kasiarakos@gmail.com>
2016-09-23 09:33:14 +0300

    EIP Message Translator (prosessor/bean/transform)

--- src/main/java/com/kasiarakos/main/Main.java
+++ src/main/java/com/kasiarakos/main/Main.java
@@ -1,5 +1,7 @@
 package com.kasiarakos.main;	
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +9,10 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("camelContext.xml");
+		
+		CamelContext camelContext = context.getBean(CamelContext.class);
+		ProducerTemplate template = camelContext.createProducerTemplate();
+		template.sendBody("direct:start","Title: Hello World \n kasiarakos test \n This is the end");
 		
 		context.start();
 		Thread.sleep(10000);
